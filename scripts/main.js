95ec2316a50c6d83e9e7c02527434703f9f0d182
Clinton Ramonida <cramonida@vfs.com>
2018-05-02 15:40:16 -0700

    Adding a base file/project to start with vue.js 2.0

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+/*
+Title: Main Control JS.
+
+Created on Mon Feb 19 2018.
+
+Author: Clinton Jay Ramonida.
+
+In cooperation with my Mentor - Scott Henshaw. 
+
+Copyright (c) 2018.
+All Rights Reserved.
+ */
+// ===================================================================
+'use strict';
+ 
+//Import all the necessary scripts here
+import { App } from './app.js';
+
+document.addEventListener('DOMContentLoaded', ( event ) => {
+
+    let app = new App();
+    // app.run();
+
+});
