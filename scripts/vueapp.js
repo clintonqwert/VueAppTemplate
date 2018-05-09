f1e369a2efa5ce2b9d144c40387a41f2a7ba9888
Clinton Ramonida <clinton_ramonida25@yahoo.com>
2018-05-09 15:53:53 -0700

    Tried Scotts' html.requests, works but not recognized as ajax as well.

--- scripts/vueapp.js
+++ scripts/vueapp.js
@@ -1,38 +1,4 @@
-new Vue({
-    el:'#sketch-fab',
-    
-    data:{
-        urlId: ''
-    },
-
-    methods:{
-        loadModel(){
-            let iframe = document.querySelector('#api-frame');
-            let client = new Sketchfab( iframe );
-            let model = this.$data.urlId;
-            
-            if(model === ""){
-                model = '20df9d0706da4ba09d7f95f82bb36806';
-            }
-            
-            client.init( model, 
-                {
-                success: function onSuccess( api ){
-                    api.start();
-                    api.addEventListener( 'viewerready', function() {
-                        console.log("Viewer is ready");
-                    });
-                }, 
-                error: function onError() {
-                    console.log('Viewer Error');
-                },
-                autostart: 1,
-                autospin: 0.2
-            });
-        }
-    }
-})
-
+import { HTTP } from "../scripts/http.js";
 
 new Vue({
     el:'#product-form',
@@ -47,8 +13,8 @@ new Vue({
             const url = 'server/simple_server.php';
             let data = {}
             let payload = {
-                method: "POST",
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                //method: "POST",
+                //headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
                 action: 'validate',
                 product: this.$data.product,
                 description: this.$data.description
@@ -56,13 +22,20 @@ new Vue({
             
             let params = JSON.stringify(payload);
 
+            HTTP.post( url, payload)
+                .then( ( data ) => { 
+                    console.log(1, 'success', JSON.parse( data )); 
+            });
+
+            /* 
             fetch(url, params)
                 .then((res) => {
                     let data = res['data'];
                     console.log(data);
-            }) 
+            }); 
+            */
             
-            /* //***ajax post with axios***
+             /* //***ajax post with axios***
             let config = {
                 headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
             }
@@ -70,9 +43,9 @@ new Vue({
             axios.post(url, params, config)
                 .then( (response) =>{
                     console.log(response.data);
-                }); 
+                });  
             */
-
+            
             /*  //***fetch with await/async***
             const server = "server/simple_server.php";
             let payload = {
@@ -94,6 +67,41 @@ new Vue({
             let view = new Sketchfab();
 
             view.sketchFabInit();
+        }
+    }
+})
+
+new Vue({
+    el:'#sketch-fab',
+    
+    data:{
+        urlId: ''
+    },
+
+    methods:{
+        loadModel(){
+            let iframe = document.querySelector('#api-frame');
+            let client = new Sketchfab( iframe );
+            let model = this.$data.urlId;
+            
+            if(model === ""){
+                model = '8568d9d14a994b9cae59499f0dbed21e';
+            }
+            
+            client.init( model, 
+                {
+                success: function onSuccess( api ){
+                    api.start();
+                    api.addEventListener( 'viewerready', function() {
+                        console.log("Viewer is ready");
+                    });
+                }, 
+                error: function onError() {
+                    console.log('Viewer Error');
+                },
+                autostart: 1,
+                autospin: 0.2
+            });
         }
     }
 })
